01905b00023e2531e5ef26798b1a263f62d797bc
suncaiyu <42695008+suncaiyu@users.noreply.github.com>
2020-09-07 08:14:50 +0800

    Add files via upload

---
+++ ButtonGroup/MainWindow.cpp
@@ -0,0 +1,26 @@
+﻿#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <ButtonGroup.h>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+      , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ButtonGroup *bg = new ButtonGroup(this);
+    for (int i = 0; i < 3; i++) {
+        QPushButton *btn = new QPushButton(QString::number(i));
+        bg->addButton(btn, i);
+    }
+    bg->SetHeight(35);
+    bg->setLinePosition(ButtonGroup::LinePosition_Bottom);
+    ui->verticalLayout->insertWidget(0, bg);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
